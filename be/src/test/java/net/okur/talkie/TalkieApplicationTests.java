6961227f9413c71313d1ea4f0105defdf42e3daf
dogancan.okur <dogancan.okur@gmail.com>
2023-06-03 02:17:56 +0300

    talkie backend init

---
+++ be/src/test/java/net/okur/talkie/TalkieApplicationTests.java
@@ -0,0 +1,13 @@
+package net.okur.talkie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
